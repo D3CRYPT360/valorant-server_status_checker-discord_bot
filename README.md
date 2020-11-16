3d5d80cf56bba87320ac46dda24a02b2f254311c
D3crypt360 <nahilali3600@gmail.com>
2020-11-16 21:05:52 +0500

    fixed some bugs and unnecessary code

--- README.md
+++ README.md
@@ -2,15 +2,10 @@
 
 Riot Games Status checker for valorant connected to discord via discord.py
 
-*This isn't the most efficient nor the best way to do this. But if it works it works..*
-
-**Feel free to make a PR :D**
-
-To install:
-
 ## Screenshots
-![when no maintenance/incidents](https://cdn.discordapp.com/attachments/746327425759182908/777590931934412830/unknown.png)
-![when maintenance/incidents](https://cdn.discordapp.com/attachments/746327425759182908/777591198482825236/unknown.png)
+
+![when no maintenance/incidents](https://cdn.discordapp.com/attachments/746327425759182908/777797415486357554/unknown.png)
+![when maintenance/incidents](https://cdn.discordapp.com/attachments/746327425759182908/777797140956708894/unknown.png)
 
 ## Setup
 1. Open a terminal in a folder or cd to a folder you want to clone the rep to
@@ -20,8 +15,27 @@ To install:
 git clone https://github.com/D3CRYPT360/valorant-server_status_checker-discord_bot.git
 ```
 
+3. Once you are in the folder open a terminal and cd to the folder and install the requirements using:
+```html
+pip install -r requirements.txt
+```
+4. Open `status.py` using a text editor or an IDE
+5. Replace `TOKEN = YOUR TOKEN` with your own token
+6. Replace `YOUR PREFIX` with your own prefix
+
+## Requirements
+- discord.py==1.5.1
+- requests==2.24.0
+
+## Commands
+| Commands                | Description                     |Example    |
+| ------------------------|:-------------------------------:|----------:|
+| {prefix}status {region} | Returns the status of the server| !status na|
+
+**All regions are converted to lowercase so if you type Na or LaTAm it will be accepted**
+
 ## Status
-Project is: __on hold__, since i am not focusing on this project at the moment, I hope to get som PR's to improve the code.
+Project is: __on hold__, since i am not focusing on this project at the moment, feel free to make PR's to improve the code if needed.
 
 ## Contact
 Created by DΞ𝕮ЯYPƬ#5020 - feel free to contact me on discord!
--- status.py
+++ status.py
@@ -1,226 +1,67 @@
-"""
-MIT License
-
-Copyright (c) 2020 D3crypt360
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-
 #IMPORTS
 import discord
 from discord.ext import commands
 import requests
 
-bot = commands.Bot(command_prefix = "YOUR_PREFIX")
-TOKEN = #TOKEN
+bot = commands.Bot(command_prefix = "YOUR PREFIX", case_insensitive=True)
+TOKEN = "YOUR TOKEN"
 
 @bot.event
 async def on_ready():
     print("bot is now online")
 
+thumbnail = "https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256"
 
-# NA
+
 @bot.command()
-async def na(ctx):
-    # GETS REQ FROM API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/na.json")
-    riot = r.json()
-    
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
+async def status(ctx, region):
+    region = region.lower()
+    r = requests.get(f"https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/{region}.json")
+    if r.status_code == 200:
+        json_data = r.json()
+                
+        if (json_data['incidents']) and (json_data['maintenances']) == []:# If there is no incidents/maintenance
+            await ctx.send ("No recent issues or events reported")
+                  
         
-    # TRIGERRS IF THERE IS AN INCIDENT
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-        
-    # TRIGGERS IF THERE IS A MAINTENANCE
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-
-#AP
-@bot.command()
-async def ap(ctx):
-    # GETS REQ FROM API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/ap.json")
-    riot = r.json()
+        elif (json_data['maintenances'])!= []:# If the issue is related to a maintenance
+            embed = discord.Embed(
+                colour = discord.Colour.dark_gold(),
+                title=(json_data["maintenances"][0]['titles'][0]['content'])
+            )
+            embed.add_field(name = (json_data['maintenances'][0]['updates'][0]['created_at'][:10]), value=(json_data['maintenances'][0]['updates'][0]['translations'][0]['content']))
+            embed.set_thumbnail(url=thumbnail)
+            await ctx.send(embed=embed)
+            
+        elif (json_data['incidents']) != []:# If the issue is related to an incident
+            embed = discord.Embed(
+                colour = discord.Colour.dark_gold(),
+                title=(json_data["incidents"][0]['titles'][0]['content'])
+            )
+            embed.add_field(name = (json_data['incidents'][0]['updates'][0]['created_at'][:10]), value=(json_data['incidents'][0]['updates'][0]['translations'][0]['content']))
+            embed.set_thumbnail(url=thumbnail)
+            await ctx.send(embed=embed)
+                
+        if (json_data['maintenances']) and (json_data['incidents']) != []: # If there is both a maintenance and an incident  
+            embed1 = discord.Embed(
+                colour = discord.Colour.dark_gold(),
+                title=(json_data["maintenances"][0]['titles'][0]['content'])
+            )
+            embed1.add_field(name = (json_data['maintenances'][0]['updates'][0]['created_at'][:10]), value=(json_data['maintenances'][0]['updates'][0]['translations'][0]['content']))
+            embed1.set_thumbnail(url=thumbnail)
+            
+            embed2 = discord.Embed(
+                colour = discord.Colour.dark_gold(),
+                title=(json_data["incidents"][0]['titles'][0]['content'])
+            )
+            embed2.add_field(name = (json_data['incidents'][0]['updates'][0]['created_at'][:10]), value=(json_data['incidents'][0]['updates'][0]['translations'][0]['content']))
+            embed2.set_thumbnail(url=thumbnail)
+            await ctx.send(embed=embed1)
+            await ctx.send(embed=embed2)
+            
     
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
-
-    # TRIGERRS IF THERE IS AN INCIDENT
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
+    elif r.status_code != 200:
+        await ctx.send(f"{region} server status not found...")
     
-    # TRIGGERS IF THERE IS A MAINTENANCE
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
- 
-#BR       
-@bot.command()
-async def br(ctx):
-    # GETS REQ FROM API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/br.json")
-    riot = r.json()
-    
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
-        
-    # TRIGERRS IF THERE IS AN INCIDENT
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-    
-    # TRIGGERS IF THERE IS A MAINTENANCE
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-
-#EU        
-@bot.command()
-async def eu(ctx):
-    # SENDS REQ TO API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/eu.json")
-    riot = r.json()
-    
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
-    
-    # TRIGGERS IF THERE IS AN INCIDENT
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-    
-    # TRIGGERS IF THERE IS A MAINTENANCE
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-
-#LATAM       
-@bot.command()
-async def latam(ctx):
-    
-    # SENDING REQ TO API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/latam.json")
-    riot = r.json()
-    
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
-    
-    # TRIGGERS IF THERE IS AN INCIDENT    
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-    
-    # TRIGGERS IF THERE IS A MAINTENANCE    
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-    
-#KR        
-@bot.command()
-async def kr(ctx):
-    # SENDS REQ TO API
-    r = requests.get("https://valorant.secure.dyn.riotcdn.net/channels/public/x/status/kr.json")
-    riot = r.json()
-    
-    # CHECKS FOR INCIDENTS
-    if (riot['incidents']) == []:
-        await ctx.send ("No recent issues or events reported")
-     
-    # TRIGGERS IF THERE IS AN INCIDENT    
-    elif (riot['incidents']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["incidents"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['incidents'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['incidents'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
-        
-    # TRIGGERS IF THERE IS A MAINTENANCE  
-    elif (riot['maintenances']) != []:
-        embed = discord.Embed(
-            colour = discord.Colour.dark_gold(),
-            title=(riot["maintenances"][0]['titles'][0]['content'])
-        )
-        embed.add_field(name = (riot['maintenances'][0]['updates'][0]['created_at'][:16].split("T")[0]), value=(riot['maintenances'][0]['updates'][0]['translations'][0]['content']))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/514418193364942850/7c56b3712cd14ae3db6c8593c5f23cf5.png?size=256")
-        await ctx.send(embed=embed)
 
 bot.run(TOKEN)
