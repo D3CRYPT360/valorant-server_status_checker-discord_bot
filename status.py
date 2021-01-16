ffae0838e406ec2989a608b6f4de22ac969c02c0
D3crypt360 <70775520+D3CRYPT360@users.noreply.github.com>
2021-01-16 12:50:32 +0500

    Update status.py

--- status.py
+++ status.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-from valoStatus import Region
+import DiscordUtils # https://github.com/toxicrecker/DiscordUtils
 
-bot = commands.Bot(command_prefix=",")
+bot = commands.Bot(command_prefix="PREFIX")
 TOKEN = "TOKEN"
 
 
@@ -17,22 +17,29 @@ async def status(ctx, region):
         await ctx.send("no errors")
 
     else:
-
+        
         if region.incident_check() == True:
-            embed = discord.Embed(
+            global embed1
+            embed1 = discord.Embed(
                 colour = discord.Colour.orange(),
-                title = region.incidents_title()
+                title = region.incident_title()
             )
-            embed.add_field(name=region.incidents_date(), value=region.incidents_reason())
-            await ctx.send(embed=embed)
+            embed1.add_field(name=region.incident_date(), value=region.incident_reason())
 
-        elif region.maintenence_check() == True:
-            embed = discord.Embed(
+        if region.maintenence_check() == True:
+            embed2 = discord.Embed(
                 colour = discord.Colour.orange(),
-                title = region.maintenances_title()
+                title = region.maintenance_title()
             )
-            embed.add_field(name=region.maintenances_date(), value=region.maintenances_reason())
-            await ctx.send(embed=embed)
-            
+            embed2.add_field(name=region.incident_reason(), value=region.maintenance_reason())
+
+
+            # running in a paginator so it looks cleaner
+            paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, auto_footer = True)
+            paginator.add_reaction('⬅️', "back")
+            paginator.add_reaction('➡️', "next")
+            embeds = [embed1, embed2]
+            await paginator.run(embeds)
+
 
 bot.run(TOKEN)
